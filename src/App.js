3972df640448c7a739b7e94d5a20d784f5c71a34
seanhong1215 <seanhong1215@gmail.com>
2022-08-19 22:25:59 +0800

    Day15

--- src/App.js
+++ src/App.js
@@ -5,70 +5,106 @@ import {
   NavLink,
   Routes,
   Route,
-  useNavigate 
+  useNavigate,
+  useParams,
+  Outlet
 } from 'react-router-dom';
 
+const Home = () => {
+  return (
+    <>
+      <h4>這是首頁</h4>
+      {/* <Outlet /> */}
+    </>
+    )
+}
 const Todo = () => {
   return (
   <>
-    <p>這是 Todo 頁面</p>
-    <Logout />
+    <NavLink to="/">
+            <p> &larr; Back </p>
+    </NavLink>
+    <h4>這是Todo頁面</h4>
   </>
   )
 };
-const Logout = () => {
-  const navigate = useNavigate();
-  return <button onClick = {()=>{ navigate('/login') }}>登出</button>
-}
+
 const Login = () => {
-  return <p>這是登入頁面</p>;
-};
+  return (
+  <>
+  <NavLink to="/">
+            <p> &larr; Back </p>
+    </NavLink>
+    <h4>這是登入頁面</h4>
+  </>
+  )
+}
 const Register = () => {
-  return <p>這是註冊頁面</p>;
+  return (
+    <>
+    <NavLink to="/">
+            <p> &larr; Back </p>
+    </NavLink>
+    <h4>這是註冊頁面</h4>
+    </>
+
+    )
 };
 
-const Home = () => {
-  return <p>這是首頁</p>;
-};
+const ProfilePage = () => {
+  let params = useParams();
+  return (
+    <>
+        {/* <h4>使用者詳細資料</h4> */}
+        <p>UserID: {params.userId}</p>
+       
 
+    </>
+  )
+}
+const Users = () => {
+  return (
+  <>
+    <NavLink to="/">
+            <p> &larr; Back </p>
+    </NavLink>
+            <h4>使用者詳細資料<NavLink to="123">123</NavLink></h4>
+   
+    <Outlet />
+  </>
+  )
+}
 function App() {
   return (
     <div className="container">
       <HashRouter>
         <div className="nav-link">
-          <NavLink to="/">
-            <p>回到首頁</p>
-          </NavLink>
-          <NavLink to="/register">
-            <p>註冊頁面</p>
+          
+           <NavLink to="/register">
+            <p>Register Page</p>
           </NavLink>
           <NavLink to="/login">
-            <p>登入頁面</p>
+            <p>Login Page</p>
           </NavLink>
-        
           <NavLink to="/todo">
-            <p>Todo 頁面</p>
+            <p>Todo Page</p>
+          </NavLink> 
+          <NavLink to="/users">
+            <p>Post頁面</p>
           </NavLink>
-          {/* <NavLink to="/error">
-            <p>Error 頁面</p>
-          </NavLink> */}
         </div>
         {/* Routes, Route 練習區 */}
         {/* 練習區 */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/todo" element={<Todo />} /> 
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/register" element={<Register />} /> 
-          <Route path="/logout" element={<Logout />} /> 
-          {/* <Route
-            path="*"
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>There's nothing here!</p>
-                </main>
-              }
-            /> */}
+        <Route path="/" element={<Home />} />
+            <Route path="users" element={<Users />} >
+              <Route path=":userId" element={<ProfilePage />} />
+            </Route>
+
+            <Route path="/todo" element={<Todo />} />
+            <Route path="/login" element={<Login />} /> 
+            <Route path="/register" element={<Register />} /> 
+          
         </Routes>
       </HashRouter>
     </div>
